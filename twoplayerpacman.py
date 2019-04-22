48cd7f7a48fc81fb647c686beb43b29788975991
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-04-22 14:25:20 -0400

    Update twoplayerpacman.py

--- twoplayerpacman.py
+++ twoplayerpacman.py
@@ -1,1 +1,21 @@
+from ggame import App, Sprite, RectangleAsset, LineStyle, Color
 
+
+# THIS IS HOPEFULLY ALL FOR BUILDING THE MAP
+class GenericWall(Sprite):
+    def __init__(self, x, y, w, h, color):
+        snapfunc = lambda X : X - X % w
+        super().__init__(
+            RectangleAsset(w-1,h-1,LineStyle(0,Color(0, 1.0)), color),
+            (snapfunc(x), snapfunc(y)))
+        # destroy any overlapping walls
+        collideswith = self.collidingWithSprites(type(self))
+        if len(collideswith):
+            collideswith[0].destroy()
+            
+# impenetrable wall (black)
+class Wall(GenericWall):
+    def __init__(self, x, y):
+        super().__init__(x, y, 50, myapp.height, Color(0, 1.0))
+    def step(self):
+        Wall(self.pos[0], self.pos[1])
