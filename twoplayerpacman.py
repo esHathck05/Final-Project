becf96b24a19d59d9aee1bc4f896998eaac51d45
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-04-30 14:35:21 -0400

    Updated from Brython Server: 4/30/2019 2:35:15 PM

--- twoplayerpacman.py
+++ twoplayerpacman.py
@@ -6,6 +6,7 @@ myapp = App()
 black = Color(0, 1)
 noline = LineStyle(0, black)
 yellow = Color(0xffff00, 1)
+blue = Color(0x6495ed, 1)
 
 myapp = App()
 
@@ -39,6 +40,30 @@ class Pacman(Sprite):
         elif key == "down arrow":
             self.xdirection = 0
             self.ydirection = 4
+            
+class Ghost(Sprite):
+    def __init__(self, x, y, w, h, color, app):
+        super().__init__(CircleAsset(30, LineStyle(0,Color(0, 1.0)), color))
+        self.xdirection = 4
+        self.ydirection = 0
+        self.go = True
+            
+    def move(self, key):
+        if key == "a":
+            self.ydirection = 0
+            self.xdirection = -4
+
+        elif key == "d":
+            self.ydirection = 0
+            self.xdirection = 4
+                
+        elif key == "w":
+            self.xdirection = 0
+            self.ydirection = -4
+            
+        elif key == "s":
+            self.xdirection = 0
+            self.ydirection = 4
 
 
 # Set up event handlers for the app
@@ -46,30 +71,22 @@ class Twoplayer(App):
     def __init__(self):
         super().__init__()
         self.pac = Pacman(50, 50, 10, 10, yellow, self)
+        self.ghost = Ghost(50, 50, 10, 10, blue, self)
         myapp.listenKeyEvent('keydown', 'left arrow', self.moveKey)
         myapp.listenKeyEvent('keydown', 'right arrow', self.moveKey)
         myapp.listenKeyEvent('keydown', 'up arrow', self.moveKey)
         myapp.listenKeyEvent('keydown', 'down arrow', self.moveKey)
-
-    def step(self):
-        self.pac.x += self.pac.xdirection
-        if self.pac.x + self.pac.width > myapp.width:
-            self.pac.x -= self.pac.xdirection
-            left(Pacman)
-        if self.pac.x < 0:
-            right(Pacman)
-        
-        self.pac.y += self.pac.ydirection
-        if self.pac.y + self.pac.height > myapp.height:
-            self.pac.y -= self.pac.ydirection
-            down(Pacman)
-        if self.pac.y < 0:
-            up(Pacman)
+        myapp.listenKeyEvent('keydown', 'a', self.moveKey)
+        myapp.listenKeyEvent('keydown', 'd', self.moveKey)
+        myapp.listenKeyEvent('keydown', 'w', self.moveKey)
+        myapp.listenKeyEvent('keydown', 's', self.moveKey)
     
     # handles directions
     def moveKey(self, event):
         if self.pac:
             self.pac.move(event.key)
+        if self.ghost:
+            self.ghost.move(event.key)
             
     def leftKey(event):
         left(Pacman)
@@ -82,6 +99,57 @@ class Twoplayer(App):
         
     def downKey(event):
         down(Pacman)
+        
+    
+    def leftKey(event):
+        left(Ghost)
+        
+    def rightKey(event):
+        right(Ghost)
+        
+    def upKey(event):
+        up(Ghost)
+        
+    def downKey(event):
+        down(Ghost)
+
+    def step(self):
+        self.pac.x += self.pac.xdirection
+        self.pac.y += self.pac.ydirection
+        self.ghost.x += self.ghost.xdirection
+        self.ghost.y += self.ghost.ydirection
+        
+        if self.pac.x > myapp.width:
+            self.pac.xdirection *= -1
+        
+        """
+        if self.pac.x + self.pac.width > myapp.width:
+            self.pac.x -= self.pac.xdirection
+            left(Pacman)
+        if self.pac.x < 0:
+            right(Pacman)
+        
+        self.pac.y += self.pac.ydirection
+        if self.pac.y + self.pac.height > myapp.height:
+            self.pac.y -= self.pac.ydirection
+            down(Pacman)
+        if self.pac.y < 0:
+            up(Pacman)
+            
+        self.ghost.x += self.ghost.xdirection
+        if self.ghost.x + self.ghost.width > myapp.width:
+            self.ghost.x -= self.ghost.xdirection
+            left(Ghost)
+        if self.ghost.x < 0:
+            right(Ghost)
+        
+        self.ghost.y += self.ghost.ydirection
+        if self.ghost.y + self.ghost.height > myapp.height:
+            self.ghost.y -= self.ghost.ydirection
+            down(Ghost)
+        if self.ghost.y < 0:
+            up(Ghost)
+        """
 
 app = Twoplayer()
 app.run()
