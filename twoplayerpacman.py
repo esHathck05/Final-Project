0875da98bffc6961be73f268f7d60d20ffbd18fe
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-05-23 15:19:51 -0400

    Updated from Brython Server: 5/23/2019 3:19:49 PM

--- twoplayerpacman.py
+++ twoplayerpacman.py
@@ -158,7 +158,7 @@ class Twoplayer(App):
         self.score += 1
         if self.score % 100 == 0 and self.ghost.stopscore == False:
             print(int(self.score))
-        if self.score == 300:
+        if self.score == 2000:
             self.ghost.stopscore = True
             self.ghost.pacisalive = False
             print("""
