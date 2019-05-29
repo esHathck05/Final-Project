3a6f6c82a53d8ff4456624e97e103a138d099e0c
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-05-29 13:36:05 -0400

    Updated from Brython Server: 5/29/2019 1:36:03 PM

--- twoplayerpacman.py
+++ twoplayerpacman.py
@@ -228,17 +228,18 @@ Player 1 Wins!""")
         self.obs.x += self.obs.xdirection
         self.obs.y += self.obs.ydirection
         if self.obs.y == int(myapp.height)/2 - 29 and self.obs.x == 100.5:
-            if random.randint(0, 5) == 1:
+            firstnumber = int(random.randint(0, 5))
+            if firstnumber == 1:
                 self.obs.xdirection *= 1
                 self.obs.ydirection *= 1
-            elif random.randint(0, 5) == 2:
+            elif firstnumber == 2:
                 self.obs.xdirection = 4
                 self.obs.xdirection *= -1
                 self.obs.ydirection = 0
-            elif random.randint(0, 5) == 3:
+            elif firstnumber == 3:
                 self.obs.xdirection *= -1
                 self.obs.ydirection *= -1
-            elif random.randint(0, 5) == 4:
+            elif firstnumber == 4:
                 self.obs.ydirection = 4
                 self.obs.xdirection = 0
                 self.obs.ydirection *= -1
